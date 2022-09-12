d84319e778536c533ac36e86cf82796df34095d1
Gil Tselenchuk <giltsl@Gils-MacBook-Air.local>
2022-09-12 12:53:48 +0300

    init ts

--- routes/router.js
+++
@@ -1,9 +0,0 @@
-var controller = require('../controller')
-
-exports.router = (app, io) => {
-    app.get('/messages', controller.getMessages);
-    
-    app.post('/messages', (req, res) => {
-        controller.saveMessage(req, res, io)
-    });
-}
